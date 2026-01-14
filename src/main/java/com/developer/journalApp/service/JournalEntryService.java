c30584b91909d6fc2c464182ee6930d0d4a83b75
Yogeshwar Singh <ys18102004sy@gmail.com>
2026-01-14 14:05:47 +0530

    adds sonar properties in yml

--- src/main/java/com/developer/journalApp/service/JournalEntryService.java
+++ src/main/java/com/developer/journalApp/service/JournalEntryService.java
@@ -5,8 +5,6 @@ import com.developer.journalApp.entity.User;
 import com.developer.journalApp.repository.JournalEntryRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
